da140aeca5919ac23c5085b6d84703b98bc7328b
Hernan Di Tano <hditano@gmail.com>
2023-03-03 13:02:30 +0100

    added more flask examples and non-stop of simconnect example

--- SimTest/main.py
+++ SimTest/main.py
@@ -1,5 +1,7 @@
 from SimConnect import *
 import math
+import time
+
 
 sm = SimConnect()
 
@@ -10,19 +12,27 @@ URL = 'http://maps.google.com/?q='
 altitude = aq.find("PLANE_ALTITUDE")
 altitude.time = 200
 
+while True:
 
-altitude = aq.get("PLANE_ALTITUDE")
-altitude = altitude + 1000
+    altitude = aq.get("PLANE_ALTITUDE")
+    altitude = altitude + 1000
 
-latitude = aq.get("PLANE_LATITUDE")
-longitude = aq.get("PLANE_LONGITUDE")
-title = aq.get("TITLE")
-throttle = aq.get("GENERAL_ENG_THROTTLE_LEVER_POSITION:1")
 
-print(f'Latitude: {latitude} | longitude: {longitude}')
-print(f'http://maps.google.com/?q={latitude},{longitude}')
-print(title)
-print(f'Throttle: {math.trunc(throttle)}%')
 
+    latitude = aq.get("PLANE_LATITUDE")
+    longitude = aq.get("PLANE_LONGITUDE")
+    title = aq.get("TITLE")
+    throttle = aq.get("GENERAL_ENG_THROTTLE_LEVER_POSITION:1")
+    altitude = aq.get("PLANE_ALTITUDE")
+
+    starttime = time.time()
+
+
+    print(f'Latitude: {latitude} | longitude: {longitude}')
+    print(f'http://maps.google.com/?q={latitude},{longitude}')
+    print(f'Altitude: {math.trunc(altitude)}ft')
+    print(title)
+    print(f'Throttle: {math.trunc(throttle)}%')
+    time.sleep(5.0 - ((time.time() - starttime) % 5.0))
 sm.exit()
 quit()
